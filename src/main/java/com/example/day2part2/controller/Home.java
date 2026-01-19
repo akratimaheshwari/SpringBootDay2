28e591b7cf4ed8c6399e9d487bf66ba2ceb56c22
akratimaheshwari <akratimaheshwari634@gmail.in>
2026-01-19 15:22:05 +0530

    add 5 students data to display

--- src/main/java/com/example/day2part2/controller/Home.java
+++ src/main/java/com/example/day2part2/controller/Home.java
@@ -4,12 +4,23 @@ import com.example.day2part2.models.StudentModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.*;
+
 @RestController
 public class Home {
     @GetMapping("/")
-    public StudentModel getStudent() {
-        StudentModel student = new StudentModel(1,"Akrati","abc@gmail.com");
-        return student;
+    public List<StudentModel> getStudent() {
+        List<StudentModel> students = new ArrayList<>();
+
+        students.add(new StudentModel(1, "Akrati", "akrati@gmail.com"));
+        students.add(new StudentModel(2, "Rahul", "rahul@gmail.com"));
+        students.add(new StudentModel(3, "Neha", "neha@gmail.com"));
+        students.add(new StudentModel(4, "Aman", "aman@gmail.com"));
+        students.add(new StudentModel(5, "Priya", "priya@gmail.com"));
+
+        return students;
     }
+
 
 }
